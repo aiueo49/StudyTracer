3a126db38410040adc586d9388e644452efdca2b
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-02-14 22:56:00 +0900

    :wastebasket: テストコードの削除

--- backend/bot.rb
+++ backend/bot.rb
@@ -4,10 +4,11 @@ require "discordrb"
 # Create a bot with a token
 bot = Discordrb::Bot.new token: ENV['DISCORD_BOT_TOKEN'], client_id: ENV['DISCORD_CLIENT_ID']
 
-# Respond to "Ping!" with "Pong!"
-bot.message(with_text: "Ping!") do |event|
-  event.respond "Pong!"
-end
+# テストのためのコードのため、コメントアウト
+# # Respond to "Ping!" with "Pong!"
+# bot.message(with_text: "Ping!") do |event|
+#   event.respond "Pong!"
+# end
 
 # bot start
 bot.run
