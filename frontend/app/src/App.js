597d52a7bc33b6ae733aeb3a487bc5e0d142cfea
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-02-16 21:33:13 +0900

    :tada: ヘッダー領域の確保と配色

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
+import Header from './Header';
 
 function App() {
   return (
     <Router>
+      <Header /> {/* Headerコンポーネントを挿入 */}
       <Routes>
-      	<Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
