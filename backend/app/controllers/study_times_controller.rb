07cc8933b3b8f4228496d40f94ef52447dc3810b
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-02-21 09:15:20 +0900

    :tada: 今日、今週、今までの合計学習時間の３パターンを計算しフロントで表示する機能の実装

--- backend/app/controllers/study_times_controller.rb
+++ backend/app/controllers/study_times_controller.rb
@@ -1,6 +1,43 @@
+# class StudyTimesController < ApplicationController
+#   def show
+#     study_times = StudyTime.where(user_id: params[:id])
+#     total_study_time = 0
+
+#     study_times.each do |record|
+#       if record.end_time.present? && record.start_time.present?
+#         study_time = (record.end_time - record.start_time) / 3600
+#         total_study_time += study_time
+#       end
+#     end
+
+#     # 小数点以下を切り捨てて、時間と分に分ける
+#     hours = total_study_time.floor
+#     minutes = ((total_study_time - hours) * 60).round
+
+#     render json: { study_time: "#{hours}時間#{minutes}分" }
+#   end
+# end
+
 class StudyTimesController < ApplicationController
   def show
-    study_times = StudyTime.where(user_id: params[:id])
+    # 今までの合計勉強時間を取得する
+    total_study_times = StudyTime.where(user_id: params[:id])
+    total_study_time = calculate_study_time(total_study_times)
+
+    # 今日の合計学習時間を取得する
+    today_study_times = StudyTime.where(user_id: params[:id], start_time: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    today_study_time = calculate_study_time(today_study_times)
+
+    # 今週の合計学習時間を取得する
+    this_week_study_times = StudyTime.where(user_id: params[:id], start_time: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week)
+    this_week_study_time = calculate_study_time(this_week_study_times)
+
+    render json: { total_study_time: total_study_time, today_study_time: today_study_time, this_week_study_time: this_week_study_time }
+  end
+
+  private
+
+  def calculate_study_time(study_times)
     total_study_time = 0
 
     study_times.each do |record|
@@ -14,6 +51,6 @@ class StudyTimesController < ApplicationController
     hours = total_study_time.floor
     minutes = ((total_study_time - hours) * 60).round
 
-    render json: { study_time: "#{hours}時間#{minutes}分" }
+    "#{hours}時間#{minutes}分"
   end
 end
