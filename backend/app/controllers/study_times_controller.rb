3fef90c95180b8726a40b31dbe760874bb1aaccd
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-02-25 18:31:41 +0900

    :recycle: コメントアウトしていたshowアクションを削除

--- backend/app/controllers/study_times_controller.rb
+++ backend/app/controllers/study_times_controller.rb
@@ -1,23 +1,3 @@
-# class StudyTimesController < ApplicationController
-#   def show
-#     study_times = StudyTime.where(user_id: params[:id])
-#     total_study_time = 0
-
-#     study_times.each do |record|
-#       if record.end_time.present? && record.start_time.present?
-#         study_time = (record.end_time - record.start_time) / 3600
-#         total_study_time += study_time
-#       end
-#     end
-
-#     # 小数点以下を切り捨てて、時間と分に分ける
-#     hours = total_study_time.floor
-#     minutes = ((total_study_time - hours) * 60).round
-
-#     render json: { study_time: "#{hours}時間#{minutes}分" }
-#   end
-# end
-
 class StudyTimesController < ApplicationController
   def show
     # 今までの合計勉強時間を取得する
