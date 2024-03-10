8af65ffa33f531f2bed01b59ae50de03447a68d6
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-03-11 05:41:20 +0900

    :bookmark: ユーザーに紐づいた学習時間の取得に成功

--- backend/app/controllers/study_times_controller.rb
+++ backend/app/controllers/study_times_controller.rb
@@ -1,26 +1,32 @@
 class StudyTimesController < ApplicationController
   def show
+    # ①そのトークンを持つユーザーを取得する
+    user = User.find_by(token: params[:token])
+    # ②以下、そのユーザーIDを含む学習時間を取得する
     # 今までの合計勉強時間を取得する
-    total_study_times = StudyTime.where(user_id: params[:id])
+    total_study_times = StudyTime.where(user_id: user.id)
     total_study_time = calculate_study_time(total_study_times)
 
     # 今日の合計学習時間を取得する
-    today_study_times = StudyTime.where(user_id: params[:id], start_time: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    today_study_times = StudyTime.where(user_id: user.id, start_time: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
     today_study_time = calculate_study_time(today_study_times)
 
     # 今週の合計学習時間を取得する
-    this_week_study_times = StudyTime.where(user_id: params[:id], start_time: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week)
+    this_week_study_times = StudyTime.where(user_id: user.id, start_time: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week)
     this_week_study_time = calculate_study_time(this_week_study_times)
 
     # 先週の合計学習時間を取得する
-    last_week_study_times = StudyTime.where(user_id: params[:id], start_time: Time.zone.now.prev_week.beginning_of_week..Time.zone.now.prev_week.end_of_week)
+    last_week_study_times = StudyTime.where(user_id: user.id, start_time: Time.zone.now.prev_week.beginning_of_week..Time.zone.now.prev_week.end_of_week)
     last_week_study_time = calculate_study_time(last_week_study_times)
 
     render json: { total_study_time: total_study_time, today_study_time: today_study_time, this_week_study_time: this_week_study_time, last_week_study_time: last_week_study_time }
   end
 
   def show_by_date
-    study_times = StudyTime.where(user_id: params[:id], start_time: params[:date].to_date.beginning_of_day..params[:date].to_date.end_of_day)
+    # ①そのトークンを持つユーザーを取得する
+    user = User.find_by(token: params[:token])
+    # ②以下、そのユーザーIDを含む学習時間を取得する
+    study_times = StudyTime.where(user_id: user.id, start_time: params[:date].to_date.beginning_of_day..params[:date].to_date.end_of_day)
     study_time = calculate_study_time(study_times)
     render json: { study_time: study_time }
   end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # resources :users, only: [:show]
   # トークンを含むユーザーページのURLを作成する
   get 'users/:token', to: 'users#show', as: 'user'
-  resources :study_times, only: [:show] do
+  # トークンを含むStudyTimeのURLを作成する
+  get 'study_times/:token', to: 'study_times#show', as: 'study_time'
+  resources :study_times, only: [], param: :token do
     get ':date', to: 'study_times#show_by_date', on: :member, as: :show_by_date
   end
 end
